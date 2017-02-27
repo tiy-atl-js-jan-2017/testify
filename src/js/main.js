9c347b929b0720d9f8259fff3fa8ee6cab79d279
Chris Coralli <ccoralli@gmail.com>
2017-02-27 14:49:33 -0500

    cleaned up backstagePass.

--- src/js/main.js
+++ src/js/main.js
@@ -27,24 +27,23 @@ GildedRose.prototype.normal = function()  {
 };
 
 GildedRose.prototype.backstagePass = function () {
-
+  this.sellIn -= 1;
   if (this.quality < 50) {
     this.quality += 1;
   }
-  if (this.sellIn < 11 && this.quality < 50) {
+  if (this.sellIn < 10 && this.quality < 50) {
     this.quality += 1;
   }
-  if (this.sellIn < 6 && this.quality < 50) {
+  if (this.sellIn < 5 && this.quality < 50) {
     this.quality += 1;
   }
-  this.sellIn -= 1;
   if (this.sellIn < 0)  {
     this.quality = 0;
   }
 };
 
 GildedRose.prototype.agedBrie = function()  {
-  this.sellIn = this.sellIn - 1;
+  this.sellIn -= 1;
   if (this.quality < 50) {
     this.quality += 1;
   }
