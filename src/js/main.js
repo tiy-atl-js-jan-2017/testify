4ceb83457ca4507bdc1211d6a0cc81781e2c950e
Chris Coralli <ccoralli@gmail.com>
2017-02-27 14:51:52 -0500

    cleaned up tick.

--- src/js/main.js
+++ src/js/main.js
@@ -56,38 +56,15 @@ GildedRose.prototype.tick = function () {
 
   if (this.name === 'Conjured Mana Cake') {
     this.conjuredCake();
-  } else {
-    if (this.name === 'normal') {
-      this.normal();
-    } else {
-      if (this.name === 'Backstage passes to a TAFKAL80ETC concert')  {
-        this.backstagePass();
-      } else {
-        if (this.name === 'Aged Brie')  {
-          this.agedBrie();
-        } else {
-          if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.quality > 0) {
-              if (this.name != 'Sulfuras, Hand of Ragnaros') {
-              }
-            }
-          }
-          if (this.name != 'Sulfuras, Hand of Ragnaros') {
-            this.sellIn = this.sellIn - 1;
-          }
-          if (this.sellIn < 0) {
-            if (this.name != 'Aged Brie') {
-              if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.quality > 0) {
-                  if (this.name != 'Sulfuras, Hand of Ragnaros') {
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+  }
+  if (this.name === 'normal') {
+    this.normal();
+  }
+  if (this.name === 'Backstage passes to a TAFKAL80ETC concert')  {
+    this.backstagePass();
+  }
+  if (this.name === 'Aged Brie')  {
+    this.agedBrie();
   }
 };
 
