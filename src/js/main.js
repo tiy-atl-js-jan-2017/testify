dc48b9f3329fde0a72951ca83e3341f6ffa2f6ff
Cory Vaughan <codercory13@gmail.com>
2017-02-27 14:12:40 -0500

    Passed test 28 and 30.

--- src/js/main.js
+++ src/js/main.js
@@ -48,6 +48,13 @@ GildedRose.prototype.tick = function () {
       }
     }
   }
-}
+   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert' && this.name != 'Sulfuras, Hand of Ragnaros') {
+     if (this.quality > 0) {
+       if (this.name == 'Conjured Mana Cake') {
+         this.quality = this.quality - 2;
+       }
+     }
+   }
+ }
 
 export { GildedRose };
