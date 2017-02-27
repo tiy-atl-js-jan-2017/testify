9abc82e1af326eaf70fb1f82810e5f7a499d603a
Chris Coralli <ccoralli@gmail.com>
2017-02-27 14:45:36 -0500

    aged brie prototype working

--- src/js/main.js
+++ src/js/main.js
@@ -43,6 +43,16 @@ GildedRose.prototype.backstagePass = function () {
   }
 };
 
+GildedRose.prototype.agedBrie = function()  {
+  this.sellIn = this.sellIn - 1;
+  if (this.quality < 50) {
+    this.quality += 1;
+  }
+  if (this.sellIn <= 0 && this.quality < 50)  {
+    this.quality += 1;
+  }
+};
+
 GildedRose.prototype.tick = function () {
 
   if (this.name === 'Conjured Mana Cake') {
@@ -54,30 +64,26 @@ GildedRose.prototype.tick = function () {
       if (this.name === 'Backstage passes to a TAFKAL80ETC concert')  {
         this.backstagePass();
       } else {
-        if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.quality > 0) {
-            if (this.name != 'Sulfuras, Hand of Ragnaros') {
-            }
-          }
+        if (this.name === 'Aged Brie')  {
+          this.agedBrie();
         } else {
-          if (this.quality < 50) {
-            this.quality = this.quality + 1;
-          }
-        }
-        if (this.name != 'Sulfuras, Hand of Ragnaros') {
-          this.sellIn = this.sellIn - 1;
-        }
-        if (this.sellIn < 0) {
-          if (this.name != 'Aged Brie') {
-            if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-              if (this.quality > 0) {
-                if (this.name != 'Sulfuras, Hand of Ragnaros') {
-                }
+          if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.quality > 0) {
+              if (this.name != 'Sulfuras, Hand of Ragnaros') {
               }
             }
-          } else {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
+          }
+          if (this.name != 'Sulfuras, Hand of Ragnaros') {
+            this.sellIn = this.sellIn - 1;
+          }
+          if (this.sellIn < 0) {
+            if (this.name != 'Aged Brie') {
+              if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.quality > 0) {
+                  if (this.name != 'Sulfuras, Hand of Ragnaros') {
+                  }
+                }
+              }
             }
           }
         }
