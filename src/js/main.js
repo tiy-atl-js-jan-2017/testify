3cfc6a8b6bf470086b3c4b2e5e456cf3aa6122fc
Cory Vaughan <codercory13@gmail.com>
2017-02-28 13:41:48 -0500

    refactored conjured mana code.

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,13 @@ function GildedRose (sellIn, quality, name) {
 }
 
 GildedRose.prototype.tick = function () {
-  if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+  if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert' && this.name != 'Sulfuras, Hand of Ragnaros') {
     if (this.quality > 0) {
-      if (this.name != 'Sulfuras, Hand of Ragnaros') {
         this.quality = this.quality - 1;
       }
     }
-  } else {
+
+    else {
     if (this.quality < 50) {
       this.quality = this.quality + 1;
       if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -28,9 +28,13 @@ GildedRose.prototype.tick = function () {
       }
     }
   }
+
+
   if (this.name != 'Sulfuras, Hand of Ragnaros') {
     this.sellIn = this.sellIn - 1;
   }
+
+
   if (this.sellIn < 0) {
     if (this.name != 'Aged Brie') {
       if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -39,16 +43,19 @@ GildedRose.prototype.tick = function () {
             this.quality = this.quality - 1;
           }
         }
-      } else {
+      }
+
+      else {
         this.quality = this.quality - this.quality;
       }
-    } else {
+    }
+
+    else {
       if (this.quality < 50) {
         this.quality = this.quality + 1;
       }
     }
   }
-   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert' && this.name != 'Sulfuras, Hand of Ragnaros') {
      if (this.name == 'Conjured Mana Cake') {
        if (this.quality > 0 && this.sellIn > 0) {
          this.quality = this.quality - 1;
@@ -57,7 +64,7 @@ GildedRose.prototype.tick = function () {
          this.quality = this.quality - 2;
     }
    }
- }
+ 
 }
 
 export { GildedRose };
