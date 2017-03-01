c23328cf0eb8ac33381801947ee21234e48146a8
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-01 07:57:16 -0500

    final submit for hard mode testify

--- src/js/main.js
+++ src/js/main.js
@@ -8,16 +8,60 @@ GildedRose.prototype.tick = function () {
   if (this.name === 'normal') {
     this.quality--;
     this.sellIn--;
-      if (this.sellIn <= 0) {
-        this.quality-= 1;
-      } else if (this.quality < 0) {
-        this.quality = 0;
-        }
+    if (this.sellIn <= 0) {
+      this.quality-= 1;
+    }
+    if (this.quality < 0) {
+      this.quality = 0;
+    }
   }
+
+  // // only one thing ever happens: a, b, c, or d
+  // if (foo) {
+  //   // thing a
+  // } else if (bar) {
+  //   // thing b
+  // } else if (baz) {
+  //   // thing c
+  // } else {
+  //   // thing d
+  // }
+  //
+  // // any number of things may happen
+  // if (foo) {
+  //   // thing a
+  // }
+  // if (bar) {
+  //   // thing b
+  // }
+  // if (baz) {
+  //   // thing c
+  // }
+
+
+  if (this.name === 'Aged Brie') {
+      if (this.sellIn > 0) {
+        this.quality = this.quality + 1
+        this.sellIn = this.sellIn - 1;
+      }
+      if (this.sellIn > 0 && this.quality > 50) {
+        this.quality = 50;
+      }
+      // if (this.sellIn > 0 && this.quality === 50) {
+      //   this.quality = 50;
+      // }
+  }
+
+        // if (this.sellIn < 0 && this.quality < 50) {
+        // this.quality = this.quality + 1;}
+        //   if (this.sellIn < 0 && this.quality === 49) {
+        //   this.quality = 50;
+        //   }
+
+
+
 };
-
-
-//   } === 'normal' && )
+//   } === 'normal' && )  && this.quality = 50
 //
 //   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
 //     if (this.quality > 0) {
