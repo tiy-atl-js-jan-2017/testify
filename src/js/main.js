e46383bcaaa17c34c457a1f5e8a2c402413f7a19
Brit Butler <redline6561@gmail.com>
2017-03-01 10:20:54 -0500

    Refactor Backstage passes.

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,15 @@ GildedRose.prototype.agedBrieTick = function () {
 
 GildedRose.prototype.sulfurasTick = function () {};
 
+GildedRose.prototype.backstageTick = function () {
+  this.sellIn -= 1;
+  this.quality += 1;
+  if (this.sellIn < 10) { this.quality += 1; }
+  if (this.sellIn < 5) { this.quality += 1; }
+  if (this.sellIn < 0) { this.quality = 0; }
+  if (this.quality > 50) { this.quality = 50; }
+};
+
 GildedRose.prototype.tick = function () {
   if (this.name === 'normal') {
     this.normalTick();
@@ -33,10 +42,10 @@ GildedRose.prototype.tick = function () {
     this.sulfurasTick();
     return;
   }
-  // if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
-  //   this.backstageTick();
-  //   return;
-  // }
+  if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+    this.backstageTick();
+    return;
+  }
   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
     if (this.quality > 0) {
       if (this.name != 'Sulfuras, Hand of Ragnaros') {
