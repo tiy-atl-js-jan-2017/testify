5df30c7bc7a774db136e3e677ee14720e41fe4df
Brit Butler <redline6561@gmail.com>
2017-03-01 10:06:45 -0500

    Refactor sulfuras.

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,8 @@ GildedRose.prototype.agedBrieTick = function () {
   if (this.quality > 50) { this.quality = 50; }
 };
 
+GildedRose.prototype.sulfurasTick = function () {};
+
 GildedRose.prototype.tick = function () {
   if (this.name === 'normal') {
     this.normalTick();
@@ -27,6 +29,14 @@ GildedRose.prototype.tick = function () {
     this.agedBrieTick();
     return;
   }
+  if (this.name === 'Sulfuras, Hand of Ragnaros') {
+    this.sulfurasTick();
+    return;
+  }
+  // if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+  //   this.backstageTick();
+  //   return;
+  // }
   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
     if (this.quality > 0) {
       if (this.name != 'Sulfuras, Hand of Ragnaros') {
