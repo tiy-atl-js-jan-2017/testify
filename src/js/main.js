8eae38d451cb36a4df845a01452bf9152b4b04ea
Brit Butler <redline6561@gmail.com>
2017-03-01 10:27:05 -0500

    Refactor conjured items.

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,13 @@ GildedRose.prototype.backstageTick = function () {
   if (this.quality > 50) { this.quality = 50; }
 };
 
+GildedRose.prototype.conjuredTick = function () {
+  this.sellIn -= 1;
+  this.quality -= 2;
+  if (this.sellIn <= 0) { this.quality -= 2; }
+  if (this.quality < 0) { this.quality = 0; }
+};
+
 GildedRose.prototype.tick = function () {
   if (this.name === 'normal') {
     this.normalTick();
@@ -44,6 +51,10 @@ GildedRose.prototype.tick = function () {
   }
   if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
     this.backstageTick();
+    return;
+  }
+  if (this.name === 'Conjured Mana Cake') {
+    this.conjuredTick();
     return;
   }
   if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
